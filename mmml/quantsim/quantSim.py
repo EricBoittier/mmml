46f585a328a64e1710c42f75eb65cca78ab0a43a
EricBoittier <eric.boittier@icloud.com>
2025-03-04 16:55:20 +0100

    asdfads

--- mmml/quantsim/quantSim.py
+++ mmml/quantsim/quantSim.py
@@ -32,10 +32,44 @@ def write_json(df: pl.DataFrame, fn: str):
     with open(f"{fn}.json", "w") as f:
         json.dump(df, f, indent=4)
 
+
+def setup_charmm(data, data_dir):
+    import json
+    import os
+
+    # Create a directory for the CHARMM files
+    os.makedirs(data_dir, exist_ok=True)
+
+    from mmml.pycharmmInterface import setupRes, setupBox
+
+    temperatures = data[K_KEY]
+    densities = data[RHO_KEY]
+    pressures = data[P_KEY]
+
+    for i, (temperature, density, pressure) in enumerate(zip(temperatures, densities, pressures)):
+        # create a new directory 
+        new_dir = data_dir / f"T{temperature}K_rho{density}kg/m3_p{pressure}kPa"
+        new_dir.mkdir(parents=True, exist_ok=True)
+        # change to the new directory
+        os.chdir(new_dir)
+        setupRes.main(data["charmm_res_id"])
+        setupBox.main(density, side_length)
+        # change back to the original directory
+        os.chdir(data_dir)
+
+
+
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--residue", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+
+    output_dir = Path(args.output)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     args = parser.parse_args()
     T_RHO_DF = read_data_T_rho(args.residue, pl_loaded, parquet_fn)
     print(T_RHO_DF)
@@ -50,9 +84,8 @@ def main():
         print(i, row)
 
     # save data as json
-    write_json(original, f"{args.residue}")
-    # save data as csv
-    # original.to_csv(f"{args.residue}.csv")
+    write_json(original, output_dir / f"{args.residue}.json")
+
 
 
 if __name__ == "__main__":
