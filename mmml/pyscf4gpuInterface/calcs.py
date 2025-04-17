c19b939830eaf987fb80bbf6cd2683acc451687f
EricBoittier <eric.boittier@icloud.com>
2025-04-17 14:02:37 +0200

    .

--- mmml/pyscf4gpuInterface/calcs.py
+++ mmml/pyscf4gpuInterface/calcs.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
     output = compute_dft(args, calcs, extra)
     print(output)
     import pickle
-
+    import os
     # if args.output contains a directory, make sure it exists
     if os.path.dirname(args.output) != "":
         os.makedirs(os.path.dirname(args.output), exist_ok=True)
