9120a563b8f74c697ab702c8315d98e633da868f
EricBoittier <eric.boittier@icloud.com>
2025-04-17 16:41:31 +0200

    s

--- mmml/pycharmm_interface/import_pycharmm.py
+++ mmml/pycharmm_interface/import_pycharmm.py
@@ -6,19 +6,25 @@ import sys
 # current directory of current file
 cwd = Path(__file__).parent
 
-chmh = os.environ["CHARMM_HOME"] #= "/pchem-data/meuwly/boittier/home/charmm"
-chml = os.environ["CHARMM_LIB_DIR"] #= "/pchem-data/meuwly/boittier/home/charmm/build/cmake"
-
+chmh = None
+chml = None
+with open(cwd / ".." / ".." / "CHARMMSETUP") as f:
+    if "CHARMM_HOME" in f.read():
+        chmh = f.read().split("=")[1].strip()
+    if "CHARMM_LIB_DIR" in f.read():
+        chml = f.read().split("=")[1].strip()
+    else:
+        raise ValueError("CHARMMSETUP does not contain CHARMM_HOME or CHARMM_LIB_DIR")
 if chmh is None:
     raise ValueError("CHARMM_HOME is not set")
 if chml is None:
     raise ValueError("CHARMM_LIB_DIR is not set")
 
-print(chmh)
-print(chml)
-chmhp = Path(chmh) 
-pych = chmhp / "tool" / "pycharmm"
-sys.path.append(str(pych))
+os.environ["CHARMM_HOME"] = chmh
+os.environ["CHARMM_LIB_DIR"] = chml
+
+chmhp = Path(chmh) / "tool" / "pycharmm"
+sys.path.append(str(chmhp))
 
 import pycharmm
 CGENFF_RTF = cwd / "top_all36_cgenff.rtf"
