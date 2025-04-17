bfbd0d2cf946a9363fead32db4328a913cc78228
EricBoittier <eric.boittier@icloud.com>
2025-04-17 12:38:09 +0200

    gpu4pyscf calcs

---
+++ mmml/pyscf4gpuInterface/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum as ENUM
+
+class THEORY(ENUM):
+    DFT = 0
+    HF = 1
+    MP2 = 2
+    CCSD = 3
+    MP3 = 4
+
+class BASIS(ENUM):
+    def2_tzvpp = 0
+    def2_tzvp = 1
+
+
+class CALCS(ENUM):
+    ENERGY = 0
+    GRADIENT = 1
+    HESSIAN = 2
+    HARMONIC = 3
+    THERMO = 4
+    INTERACTION = 5
+
+all_theory = [THEORY.DFT, THEORY.HF, THEORY.MP2, THEORY.CCSD, THEORY.MP3]
+all_basis = [BASIS.def2_tzvpp, BASIS.def2_tzvp]
+all_calcs = [CALCS.ENERGY, CALCS.GRADIENT, CALCS.HESSIAN, CALCS.HARMONIC, CALCS.THERMO, CALCS.INTERACTION]
