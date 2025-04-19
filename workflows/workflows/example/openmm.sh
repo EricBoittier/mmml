68408c560c9de65676be671a8d53efc495ea2bc7
EricBoittier <eric.boittier@icloud.com>
2025-04-19 12:54:09 +0200

    ...

---
+++ workflows/workflows/example/openmm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+        module load openmm && python ~/mmml/mmml/openmm_interface/openmm-test1.py                 --temperatures 100 200 300                 --pressures 1.0 2.0 3.0                 --simulation_schedule minimization equilibration NPT                 --integrator Langevin
+            
