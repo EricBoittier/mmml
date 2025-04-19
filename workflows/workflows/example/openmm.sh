b85231e6b35824133481519c4d0b50c9798e995a
Eric Boittier <40135422+EricBoittier@users.noreply.github.com>
2025-04-19 13:06:59 +0200

    s

--- workflows/workflows/example/openmm.sh
+++ workflows/workflows/example/openmm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-        module load openmm && python ~/mmml/mmml/openmm_interface/openmm-test1.py                 --temperatures 100 200 300                 --pressures 1.0 2.0 3.0                 --simulation_schedule minimization equilibration NPT                 --integrator Langevin
-            
+module load charmm
+module load cudnn
+module load openmm 
+python ~/mmml/mmml/openmm_interface/openmm-test1.py --temperatures 100 200 300 --pressures 1.0 2.0 3.0 --simulation_schedule minimization equilibration NPT --integrator Langevin
--- workflows/workflows/example/qm-run.sh
+++ workflows/workflows/example/qm-run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-            --output qm/initial.npz --mol xyz/initial.xyz             --optimize --xc PBE0 --basis cc-pVTZ             --hessian --thermo --gradient --harmonic > qm/initial.npz
+bash ~/mmml/setup/install.sh
+uv run python ~/mmml/mmml/pyscf4gpuInterface/calcs.py --output qm/run.npz --mol xyz/run.xyz --optimize --xc PBE0 --basis cc-pVTZ --hessian --thermo --gradient --harmonic > qm/run.npz
             
