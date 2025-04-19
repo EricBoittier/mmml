68081ac67740bd17dca424d1ea7d1475d88b13ae
EricBoittier <eric.boittier@icloud.com>
2025-04-19 12:54:09 +0200

    ...

---
+++ workflows/workflows/example/qm-initial.sh
@@ -0,0 +1,2 @@
+bash ~/mmml/setup/install.sh && uv run python ~/mmml/mmml/pyscf4gpuInterface/calcs.py             --output qm/initial.npz --mol xyz/initial.xyz             --optimize --xc PBE0 --basis cc-pVTZ             --hessian --thermo --gradient --harmonic > qm/initial.npz
+            
---
+++ workflows/workflows/example/qm-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+            --output qm/initial.npz --mol xyz/initial.xyz             --optimize --xc PBE0 --basis cc-pVTZ             --hessian --thermo --gradient --harmonic > qm/initial.npz
+            
