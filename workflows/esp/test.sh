3b96b065af5d6311de49dab3204475e10f397ee5
EricBoittier <eric.boittier@icloud.com>
2025-08-25 21:25:10 +0200

    .

---
+++ workflows/esp/test.sh
@@ -0,0 +1,14 @@
+#  --charge CHARGE
+#  --energy
+#  --optimize
+#  --gradient
+#  --hessian
+#  --harmonic
+#  --thermo
+#  --interaction
+#  --dens_esp
+#  --ir
+#  --shielding
+#  --polarizability
+
+python ~/mmml/mmml/pyscf4gpuInterface/calcs.py --mol test.xyz --energy --gradient --dens_esp 
