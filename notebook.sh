d7316937b9fbda3335f6aea7094c67f0d187ab4e
Khamlek Chaton <khamlekchaton493@gmail.com>
2025-09-15 17:18:47 +0200

    added all other files

--- notebook.sh
+++ notebook.sh
@@ -1,1 +1,3 @@
 module purge && source .venv/bin/activate && module load cudnn; module load gcc; module load charmm && uv run --with jupyter jupyter lab --no-browser --port 9856
+module purge && source .venv/bin/activate && module load openmm; module load gcc; module load charmm && uv run --with jupyter jupyter lab --no-browser --port 9856
+module purge && source .venv/bin/activate && module load gcc; module load charmm && uv run --with jupyter jupyter lab --no-browser --port 9856
