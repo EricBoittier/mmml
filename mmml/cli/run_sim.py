edd07f24bc34619c063a40c158c9dfaf6998d18c
EricBoittier <eric.boittier@icloud.com>
2025-09-28 18:21:29 +0200

    s

--- mmml/cli/run_sim.py
+++ mmml/cli/run_sim.py
@@ -460,7 +460,7 @@ inbfrq -1 imgfrq -1
                     Stationary(ase_atoms)
                     ZeroRotation(ase_atoms)
                     MaxwellBoltzmannDistribution(ase_atoms, temperature_K=temperature)
-                print(f"Temperature adjusted to: {temperature} K")
+                    print(f"Temperature adjusted to: {temperature} K")
             if i % 100 == 0:
                 print(f"step {i:5d} epot {potential_energy[i]: 5.3f} ekin {kinetic_energy[i]: 5.3f} etot {total_energy[i]: 5.3f}")
 
