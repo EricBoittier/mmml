d52b9cf9df9ba024d53da7f1ed7ea968442f1142
EricBoittier <eric.boittier@icloud.com>
2025-10-29 15:43:18 +0100

    asdf

---
+++ mmml/dcmnet2/dcmnet/plotting_3d.py
@@ -0,0 +1,43 @@
+import numpy as np
+from ase import Atoms
+from ase.visualize import view
+
+from .modules import NATOMS
+
+
+def plot_3d_molecule(batch, batch_size):
+    i = 0
+    b1_ = batch["Z"].reshape(batch_size, NATOMS)[i]
+    c1_ = batch["mono"].reshape(batch_size, NATOMS)[i]
+    nonzero = np.nonzero(c1_)
+    i = 0
+    xyz = batch["R"].reshape(batch_size, NATOMS, 3)[i][nonzero]
+    elem = batch["Z"].reshape(batch_size, NATOMS)[i][nonzero]
+
+    mol = Atoms(elem, xyz)
+    V1 = view(mol, viewer="x3d")
+    return V1, mol
+
+
+def plot_3d_models(mono, dc, dcq, batch, batch_size):
+    n_dcm = mono.shape[-1]
+    i = 0
+    b1_ = batch["Z"].reshape(batch_size, NATOMS)[i]
+    c1_ = batch["mono"].reshape(batch_size, NATOMS)[i]
+    nonzero = np.nonzero(b1_)
+    print(nonzero)
+    i = 0
+    xyz = batch["R"].reshape(batch_size, NATOMS, 3)[i][nonzero]
+    elem = batch["Z"].reshape(batch_size, NATOMS)[i][nonzero]
+
+    mol = Atoms(elem, xyz)
+    V1 = view(mol, viewer="x3d")
+    idx = len(nonzero[0]) * n_dcm
+    print(idx)
+    dcmol = Atoms(["X" if _ > 0 else "He" for _ in dcq[i][:idx]], 
+                  dc[:idx])
+    V2 = view(dcmol, viewer="x3d")
+    combined = dcmol + mol
+    print(combined)
+    V3 = view(combined, viewer="x3d")
+    return V1, V2, V3, mol, dcmol, combined
