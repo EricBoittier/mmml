fc31fda6ccc3bd6d1b856fcd4b5354441440b013
EricBoittier <eric.boittier@icloud.com>
2025-10-30 16:53:24 +0100

    xcbf

--- mmml/mmml/dcmnet/analysis/demo_analysis_2.py
+++
@@ -1,9 +0,0 @@
-from dcmnet.analysis import *
-
-params = "/pchem-data/meuwly/boittier/home/jaxeq/all_runs/runs7/"\
-"20240830-171111dcm-2-espw-1000-qm9-esp40000-0.npz-re-False/best_1000_params.pkl"
-
-path = Path("/pchem-data/meuwly/boittier/home/jaxeq/misc")
-paths = list(path.glob("*"))
-for path in paths:
-    dcmnet(path, params)
