1f810da7e173d6c14a43d1ed3ab913c65ac2231b
EricBoittier <eric.boittier@icloud.com>
2025-10-30 16:53:24 +0100

    xcbf

--- mmml/dcmnet2/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Distributed charge multipole network utilities."""
-
-# The package exposes submodules such as :mod:`mmml.dcmnet.dcmnet`, but we avoid
-# importing them eagerly to keep optional dependencies lazy.  Users can import
-# the submodules explicitly (``from mmml.dcmnet import dcmnet``) without the
-# heavy side effects that ``from . import *`` introduced.
-
-__all__: list[str] = []
