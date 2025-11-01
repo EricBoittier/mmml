48aa242e29778b40c7fd5bac937d6576e240cf9a
Eric Boittier <boitti0000@login12.deploy.int>
2025-11-01 12:07:18 +0100

    asdf

---
+++ examples/co2/physnet_train/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=GPU_JOB
+#SBATCH --time=01:00:00
+#SBATCH --qos=gpu6hours
+#SBATCH --mem-per-cpu=20G
+#SBATCH --ntasks=1
+#SBATCH --array=0 #-1983
+#SBATCH --cpus-per-task=2
+#SBATCH --partition=titan #a100  # or titanx
+#SBATCH --gres=gpu:1        # --gres=gpu:2 for two GPU, etc
+
+
+module load CUDA/12.2.0
+source ~/mmml/.venv/bin/activate
+
+X=$SLURM_ARRAY_TASK_ID
+echo $X
+#python ~/mmml/mmml/pyscf4gpuInterface/calcs.py --mol "/scicore/home/meuwly/boitti0000/data/wateresp/xyzs/water_"$X".xyz" --energy --gradient --dens_esp --xc PBE0 --basis 6-31g --output "output/"$X".pkl"
+
+uv run bash train_with_atomic_refs.sh 
+
