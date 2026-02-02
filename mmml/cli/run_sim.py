8ece3bab5c964472241b522e284b354d1edd69f1
EricBoittier <eric.boittier@icloud.com>
2026-02-02 12:36:43 +0100

    Enhance output in `run_sim.py` by including forces, potential energy, kinetic energy, and total energy in the trajectory writing process. This update improves the detail and accuracy of simulation results.

--- mmml/cli/run_sim.py
+++ mmml/cli/run_sim.py
@@ -579,6 +579,10 @@ inbfrq -1 imgfrq -1
                     # Wrap for output only (does not affect integration; preserves NVE)
                     atoms_to_write = ase_atoms.copy()
                     atoms_to_write.set_positions(wrap_positions_for_pbc(ase_atoms.get_positions()))
+                    atoms_to_write.set_forces(wrap_positions_for_pbc(ase_atoms.get_forces()))
+                    atoms_to_write.set_potential_energy(potential_energy[i])
+                    atoms_to_write.set_kinetic_energy(kinetic_energy[i])
+                    atoms_to_write.set_total_energy(total_energy[i])
                     traj.write(atoms_to_write)
                 else:
                     traj.write(ase_atoms)
