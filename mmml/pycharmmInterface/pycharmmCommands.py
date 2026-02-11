9ffda289b8ee15f0c6c28aa6e99838ac4206d0d8
EricBoittier <eric.boittier@icloud.com>
2026-02-11 13:39:25 +0100

    Replace `stream` command with `crystal read` in `pycharmmCommands.py` to enhance clarity in the crystal image processing workflow. This update improves the readability and maintainability of the code.

--- mmml/pycharmmInterface/pycharmmCommands.py
+++ mmml/pycharmmInterface/pycharmmCommands.py
@@ -254,7 +254,7 @@ if @zdim .gt. @bsiz then
    set bsiz = @zdim
 endif
 
-stream crystal_image.str
+crystal read crystal_image.str
 crystal defi cubic @bsiz @bsiz @bsiz 90. 90. 90.
 crystal build 
 image byres xcen 0.0 ycen 0.0 zcen 0.0 sele all end"""
