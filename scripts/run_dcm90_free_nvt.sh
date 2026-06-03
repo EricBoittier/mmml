3e65bcedb61f736dff4b1a9fed658d2adbb7c571
EricBoittier <eric.boittier@icloud.com>
2026-06-03 18:41:48 +0200

    Adjust DCM simulation parameters for improved dynamics and output granularity
    - Changed the `--dt-fs` parameter from 0.5 to 0.25 in `run_dcm90_free_nvt.sh` to refine the time step for better simulation accuracy.
    - Updated the `--dyn-nprint` parameter from 40 to 1000 to enhance the frequency of dynamic output, providing more detailed simulation data.

--- scripts/run_dcm90_free_nvt.sh
+++ scripts/run_dcm90_free_nvt.sh
@@ -53,10 +53,10 @@ uv run mmml md-system \
   --flat-bottom-selection "TYPE C" \
   --flat-bottom-k 0.00001 \
   --temperature 200.0 --bonded-mm-mini --bonded-mm-mini-after mini,heat --bonded-mm-mini-steps 500 \
-  --dt-fs 0.5 \
+  --dt-fs 0.25 \
   --ps-heat 100 \
   --dcd-nsavc 1000 \
-  --dyn-nprint 40 --ml-switch-width 0.1 --mm-switch-width 3.0 --charmm-sd-steps 2000 --charmm-abnr-steps 200  \
+  --dyn-nprint 1000 --ml-switch-width 0.1 --mm-switch-width 3.0 --charmm-sd-steps 2000 --charmm-abnr-steps 200  \
   --ml-batch-size 2256 \
   --skip-energy-show \
   --seed 123 \
