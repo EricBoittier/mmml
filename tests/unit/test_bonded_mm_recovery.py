5101d220c67458bd9efa75512b81f64647e3f93b
Eric Boittier <40135422+EricBoittier@users.noreply.github.com>
2026-07-05 11:22:38 +0200

    test: mock additional mlpot setup functions in bonded recovery unit tests

--- tests/unit/test_bonded_mm_recovery.py
+++ tests/unit/test_bonded_mm_recovery.py
@@ -947,6 +947,12 @@ def test_assert_mlpot_user_active_error_includes_callback_pair_failure():
     ), patch(
         "mmml.interfaces.pycharmmInterface.mlpot.setup._probe_mlpot_hybrid_energy_kcal",
         return_value=0.0,
+    ), patch(
+        "mmml.interfaces.pycharmmInterface.mlpot.setup._fortran_mlpot_callback_active",
+        return_value=None,
+    ), patch(
+        "mmml.interfaces.pycharmmInterface.mlpot.setup._read_mlpot_charmm_energy_terms_kcal",
+        return_value={},
     ), patch.object(ctx, "reregister_mlpot"), patch(
         "mmml.interfaces.pycharmmInterface.mlpot.cli_common.light_resync_mlpot_state",
         return_value=0.0,
