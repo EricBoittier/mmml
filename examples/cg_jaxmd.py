961436f569b0cc51d43ff55b5495beff368edab9
EricBoittier <eric.boittier@icloud.com>
2026-07-07 18:10:32 +0200

    chore: increase BOX_SIDE_A to 30.0 in cg_jaxmd example

--- examples/cg_jaxmd.py
+++ examples/cg_jaxmd.py
@@ -74,7 +74,7 @@ NVE_TOTAL_STEPS = 20000
 NVE_BLOCK_STEPS = 100
 FIRE_CYCLES=10
 NWATER = 1500
-BOX_SIDE_A = 28.0
+BOX_SIDE_A = 30.0
 
 # 2. Build the initial system in PyCHARMM and minimize
 print("--- Building Trialanine Water Box in CHARMM ---")
