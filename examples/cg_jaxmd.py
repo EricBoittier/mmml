c9855d39cc453115285e5b57377f8646e626e817
EricBoittier <eric.boittier@icloud.com>
2026-07-07 11:59:31 +0200

    feat: improve calculator attribute extraction with fallback support for PhysNet models

--- examples/cg_jaxmd.py
+++ examples/cg_jaxmd.py
@@ -75,8 +75,11 @@ nb_settings = _nbond_settings_from_cutoffs(box.nbond_cutoffs)
 nbond_data = load_nonbonded_system_from_charmm(psf_path, prm_path)
 
 calc = create_calculator_from_checkpoint(CKPT_PATH)
-model = calc.model
-params = calc.params
+model = getattr(calc, "model", getattr(calc, "_mmml_physnet_model", None))
+params = getattr(calc, "params", getattr(calc, "_mmml_physnet_params", None))
+
+if model is None or params is None:
+    raise ValueError("Could not extract model or params from the loaded calculator.")
 
 # Setup monomer groupings (first 42 atoms: peptide; then groups of 3 for water)
 n_trialanine = 42
